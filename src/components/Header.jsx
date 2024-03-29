7fcc29ac67a2e5ae1841c2c0237ff1180d5b158c
Bruno M <brunomoron56@gmail.com>
2024-03-28 22:11:09 -0300

    adding materialUI and a sidemenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,27 @@
+import  SwipeableDrawer from "@mui/material/SwipeableDrawer"
+import { useState } from "react";
+
+function  openDrawerHandler() {
+ console.log("open");
+}
+
+function closeDrawerHandler(){
+ console.log('close')
+}
+
 function Header() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <header className="bg-black font-bold">
+        <button onClick={() => setOpen(!isOpen)}  >open</button>
+        <SwipeableDrawer className="w-9 bg-slate-500" onOpen={openDrawerHandler} onClose={closeDrawerHandler} open={isOpen}  >
+          <ul >
+            <li>hola</li>
+            <li>como</li>
+            <li>estas</li>
+          </ul>
+        </SwipeableDrawer>
         <h1 className="text-green-600 text-3xl p-4 font-mono">
             Twentytrheef
         </h1>
